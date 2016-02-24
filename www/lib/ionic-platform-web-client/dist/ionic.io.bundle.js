7f03d8a9445370c3dc5d3f0ed65ffae622c7d95f
Jeremy PRIEUR <jeremyprieur91@gmail.com>
2016-02-24 23:41:54 +0100

    dev-mode : false

--- www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
+++ www/lib/ionic-platform-web-client/dist/ionic.io.bundle.js
@@ -3439,7 +3439,7 @@ var BaseSettings = (function () {
 })();
 
 var temp = new BaseSettings().factory('$ionicCoreSettings', function () {
-  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"014424ef","api_key":"550b1ef150dc5dd9c2a9e180ddcf41789968e896c9fecc50","dev_push":true}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
+  "IONIC_SETTINGS_STRING_START";var settings = {"app_id":"014424ef","api_key":"550b1ef150dc5dd9c2a9e180ddcf41789968e896c9fecc50","dev_push":false}; return { get: function(setting) { if (settings[setting]) { return settings[setting]; } return null; } };"IONIC_SETTINGS_STRING_END";
 }).finish();
 
 var Settings = (function () {

8c4ba8834ccbcec4aa06b9ba0c345e307b8155e8
Jeremy PRIEUR <jeremyprieur91@gmail.com>
2016-02-24 20:23:19 +0100

    add iOS platform & touch id auth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,21 @@
 angular.module('starter.controllers', [])
 
 
-.controller('HomeCtrl', function($scope, $state, $ionicHistory) {
+.controller('HomeCtrl', function($scope, $state, $ionicHistory, $cordovaTouchID, $ionicPlatform) {
 
   $ionicHistory.clearHistory();
+
+  $ionicPlatform.ready(function() {
+        $cordovaTouchID.checkSupport().then(function() {
+            $cordovaTouchID.authenticate("You must authenticate").then(function() {
+                alert("The authentication was successful");
+            }, function(error) {
+                alert(JSON.stringify(error));
+            });
+        }, function(error) {
+            alert(JSON.stringify(error));
+        });
+    });
 
   $scope.newParty = function (id) {
       $state.go('newParty');
